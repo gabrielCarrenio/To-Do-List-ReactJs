9cc20cbc24fa8c813398f20d76e1c9bf2e2b4b80
Gabriel Carreño <carrenocarlosgabriel@gmail.com>
2022-07-12 20:41:59 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Btn from "./components/Btn";
+
+function App() {
+  return (
+    <div className="App">
+      <Btn />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Btn.jsx
@@ -0,0 +1,7 @@
+export default function Btn() {
+  return (
+    <>
+      <button className="">Add</button>
+    </>
+  );
+}
